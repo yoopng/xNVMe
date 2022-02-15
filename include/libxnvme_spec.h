de9a20f048242017a5a4250b029fdabaa36b06bc
Krishna Kanth Reddy <krish.reddy@samsung.com>
2022-02-15 16:33:02 +0530

    Implemented the Vectored I/O support in the io_uring passthru backend of xNVMe
    Signed-off-by: Krishna Kanth Reddy <krish.reddy@samsung.com>

--- include/libxnvme_spec.h
+++ include/libxnvme_spec.h
@@ -1192,6 +1192,40 @@ struct xnvme_spec_cmd_common {
 XNVME_STATIC_ASSERT(sizeof(struct xnvme_spec_cmd_common) == 64, "Incorrect size")
 
 /**
+ * NVMe Command Accessor for Passthru 64byte NVMe command
+ *
+ * @struct xnvme_spec_cmd_passthru
+ */
+struct xnvme_spec_cmd_passthru {
+	/* cdw 00 */
+	uint8_t opcode;			///< OPC: Command Opcode
+	uint8_t flags;
+	uint16_t rsvd;
+
+	/* cdw 01 */
+	uint32_t nsid;			///< NSID: Namespace identifier
+
+	uint32_t cdw02;
+	uint32_t cdw03;
+
+	/* cdw 04-05 */
+	uint64_t mptr;			///< MPTR -- metadata pointer
+
+	/* cdw 06-09: */		///< DPTR -- data pointer
+	uint64_t data;
+	uint32_t metadata_len;
+	uint32_t data_len;
+
+	uint32_t cdw10;
+	uint32_t cdw11;
+	uint32_t cdw12;
+	uint32_t cdw13;
+	uint32_t cdw14;
+	uint32_t cdw15;
+};
+XNVME_STATIC_ASSERT(sizeof(struct xnvme_spec_cmd_passthru) == 64, "Incorrect size")
+
+/**
 * NVMe Command Accessor for the Sanitize command
 *
 * @struct xnvme_spec_cmd_sanitize
@@ -1665,6 +1699,7 @@ XNVME_STATIC_ASSERT(sizeof(struct xnvme_spec_znd_cmd) == 64, "Incorrect size")
 struct xnvme_spec_cmd {
 	union {
 		struct xnvme_spec_cmd_common common;
+		struct xnvme_spec_cmd_passthru passthru;
 		struct xnvme_spec_cmd_sanitize sanitize;
 		struct xnvme_spec_cmd_format format;
 		struct xnvme_spec_cmd_log log;
